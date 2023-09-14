a81903be3becfa790548af491d1bbea6d4fe71b1
Olabanji <shola.albert@gmail.com>
2023-09-14 19:05:58 +0100

    fixes

--- core/admin.py
+++ core/admin.py
@@ -1,11 +1,26 @@
 from django.contrib import admin
 from .models import *
 
+from import_export import resources
+
+from import_export.admin import ImportExportModelAdmin
+
+
 # Register your models here.
 
+class ContentResource(resources.ModelResource):
+
+    class Meta:
+        model = Content
+
+
+class ContentAdmin(ImportExportModelAdmin):
+    resource_classes = [ContentResource]
 
 admin.site.register(ContentCategory)
 admin.site.register(ContentGenre)
-admin.site.register(Content)
+admin.site.register(Content, ContentAdmin)
 admin.site.register(Episode)
 admin.site.register(WatchedContent)
+
+
